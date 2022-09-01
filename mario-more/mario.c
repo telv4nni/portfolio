49910af416b41b9adde87cc8430ab8398b4b953b
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-01 17:47:01 +0000

    Thu, Sep 1, 2022, 5:47 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -14,12 +14,19 @@ int main(void)
     for (int i=0; i<size; i++)
     {
         //Draw a Row
+        //Draw empty space
         for(int j=0; j<row; j++)
         {
             printf(" ");
         }
-                   //Draw blocks
-         for(int k=0; k > i; k++)
+                   //Draw left blocks
+        for(int k=0; k < i+1; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        //Draw right blocks
+        for(int k=0; k < i+1; k++)
         {
             printf("#");
         }
