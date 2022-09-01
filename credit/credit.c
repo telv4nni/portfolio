dbd6c0029d8dc317ce91e88e947926423a68660b
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-01 18:59:39 +0000

    Thu, Sep 1, 2022, 6:59 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,15 +6,12 @@ int main(void)
     //get card number from user
     const long card = get_long("What's the card number? ");
     long number = card;
-    long modulo;
     long check = 0;
 
     //Multiply every other digit
-    do
-    {
+for (int )
         //Get modulo from the second to last number
-        modulo = 2 * ((number / 10) % 10);
-        check += modulo;
+        check += 2 * ((number / 10) % 10);
     }
     while (number > 0);
     printf("%li \n", check);
