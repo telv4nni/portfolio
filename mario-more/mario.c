76889ff309a392e54ed7ff6617c97ac7d0fb2cb2
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-01 17:14:14 +0000

    Thu, Sep 1, 2022, 5:14 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -9,10 +9,10 @@ int main(void)
         size = get_int("Choose size of the pyramid (1-8): ");
     } while (size<1 || size>8);
 
-    //Make a line
+    //Make empty space
     for (int i=0; i<size; i++)
     {
-        printf("#");
+        printf(" ");
     }
     //Make next row
 }
