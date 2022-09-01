ea0323e0b11cedd204741346940e64c004303589
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-01 20:12:57 +0000

    Thu, Sep 1, 2022, 8:12 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -10,7 +10,7 @@ int main(void)
     long check = 0;
 
     //Multiply every other digit
-    for (int i = 0; i < 3; i++)
+    for (int i = number; i > 99; i = i / 100)
     {
         modulo = 2 * ((number / 10) % 10);
         number = number/100;
