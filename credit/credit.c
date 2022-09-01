cc91d8359c4805f36be6ab5eddd4696de76ce543
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-01 18:53:13 +0000

    Thu, Sep 1, 2022, 6:53 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,10 +6,12 @@ int main(void)
     //get card number from user
     const long card = get_long("What's the card number? ");
     long number = card;
+    long check;
+    
     //Multiply every other digit
     do
     {
-        long check = 2 * ((number / 10) % 10);
+        check = 2 * ((number / 10) % 10);
     }
     while (number > 0);
     printf("%li \n", check);
