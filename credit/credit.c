9fe5a6d16fe84ff1c0b96c4f0c766389d4c1b508
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-01 18:57:10 +0000

    Thu, Sep 1, 2022, 6:57 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,13 +7,14 @@ int main(void)
     const long card = get_long("What's the card number? ");
     long number = card;
     long modulo;
+    long check = 0;
 
     //Multiply every other digit
     do
     {
         //Get modulo from the second to last number
         modulo = 2 * ((number / 10) % 10);
-        check = modulo;
+        check += modulo;
     }
     while (number > 0);
     printf("%li \n", check);
