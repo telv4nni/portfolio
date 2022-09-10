60d477c2ef1950162a4a51da49acf354c39c93e6
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-10 13:03:39 +0000

    Sat, Sep 10, 2022, 1:03 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -21,15 +21,15 @@ int main(void)
     // TODO: Print the winner
     if (score1 > score2)
     {
-        printf("Player 1 is a winner!\n");
+        printf("Player 1 wins!\n");
     }
     else if (score2 > score1)
     {
-        printf("Player 2 is a winner!\n");
+        printf("Player 2 wins!\n");
     }
     else
     {
-        printf("It's a draw.\n");
+        printf("Tie!\n");
     }
 }
 
