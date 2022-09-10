5be9e3f267921a0c8cd433b8f0185c50f75ee548
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-10 13:28:15 +0000

    Sat, Sep 10, 2022, 1:28 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,27 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
     // Check if proper number of arguments
-    if (argv[2] =/= NULL || argv[1] == NULL)
+    if (argv[2] != NULL || argv[1] == NULL)
         {
-            printf("Usage: ./substitution key\n")
+            printf("Usage: ./substitution key\n");
             return 1;
         }
     // Check if argument is correct length
-     if (strlen(argv[1]) =/= 26)
+     if (strlen(argv[1]) != 26)
         {
-            printf("Key must contain 26 characters.\n")
+            printf("Key must contain 26 characters.\n");
             return 1;
         }
 
     string plaintext = get_string("Plaintext: ");
     string ciphertext;
-
+    printf("%c\n", argv[1][2]);
     for (int i = 0; i < strlen(argv[1]); i++)
     {
-        if ()
     }
 }
