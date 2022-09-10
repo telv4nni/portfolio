6c9dc502a6284f321c3a4bf3d230a43e4f156ad1
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-10 13:20:04 +0000

    Sat, Sep 10, 2022, 1:20 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,7 +4,10 @@
 
 int main(int argc, string argv[])
 {
+        if (argv[2] =/= NULL)
+        {
+            printf("Usage: ./substitution key")
+        }
     string plaintext = get_string("Plaintext: ");
     string ciphertext;
-    
 }
