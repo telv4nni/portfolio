7cdc02511ff7fa203fe415ad00696e46e595067b
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-10 13:24:52 +0000

    Sat, Sep 10, 2022, 1:24 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -4,10 +4,24 @@
 
 int main(int argc, string argv[])
 {
-        if (argv[2] =/= NULL)
+    // Check if proper number of arguments
+    if (argv[2] =/= NULL || argv[1] == NULL)
         {
-            printf("Usage: ./substitution key")
+            printf("Usage: ./substitution key\n")
+            return 1;
         }
+    // Check if argument is correct length
+     if (strlen(argv[1]) =/= 26)
+        {
+            printf("Key must contain 26 characters.\n")
+            return 1;
+        }
+
     string plaintext = get_string("Plaintext: ");
     string ciphertext;
+
+    for (int i = 0; i < strlen(argv[1]); i++)
+    {
+        if ()
+    }
 }
