65f9a93a5a2460c973fcceba6eb51717fe85328e
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-14 12:21:38 +0000

    Wed, Sep 14, 2022, 12:21 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -132,7 +132,7 @@ void add_pairs(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int i = 0; i < candidate_count; i++)
+        for (int j = 0; j < candidate_count; j++)
         {
             if (preferences[i][j] > preferences[j][i])
             {
