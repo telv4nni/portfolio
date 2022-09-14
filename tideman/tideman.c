eea2a3822a73662c5b3688c30bd376a973979376
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-14 12:04:37 +0000

    Wed, Sep 14, 2022, 12:04 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -116,10 +116,14 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j < candidate_count; j++)
+        for (int j = 1; j < candidate_count; j++)
         {
-            preferences[ranks[i]][j];
+            if (ranks[i] > ranks[j])
+            {
+                preferences[i][j]
+            }
         }
+
     }
     return;
 }
