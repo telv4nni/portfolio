2cb571c0724189a90c50b2109df39188deb621c4
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-14 13:46:25 +0000

    Wed, Sep 14, 2022, 1:46 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -175,10 +175,7 @@ void lock_pairs(void)
 {
    for (int i = 0; i < pair_count; i++)
     {
-        for (int j = 0; j < pair_count; j++)
-        {
-           locked[pairs[i].winner][pairs[i].loser] = 
-        }
+        locked[pairs[i].winner][pairs[i].loser] = true;
     }
     return;
 }
