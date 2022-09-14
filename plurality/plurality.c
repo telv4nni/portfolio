ed52ae148cc903871c7e59acc819a98639c20739
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-14 10:32:47 +0000

    Wed, Sep 14, 2022, 10:32 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -80,9 +80,10 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
+    string winner;
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        if (candidates[i].votes > )
     }
     return;
 }
