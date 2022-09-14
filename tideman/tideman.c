868fc63302fc7d8c9e20dc724db50004c34753d8
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-14 11:18:24 +0000

    Wed, Sep 14, 2022, 11:18 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -99,7 +99,13 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-   if (strcmp(name, ))
+    for (int i = 0; i < candidate_count; i++)
+    {
+   if (strcmp(name, candidates[i] == 0;))
+   {
+
+   }
+    }
     return false;
 }
 
