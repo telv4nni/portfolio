9357e43a46b37653e00bdd4afdc895e203422429
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-14 10:23:29 +0000

    Wed, Sep 14, 2022, 10:23 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,7 +68,7 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, candidates[i].name))
+        if (strcmp(name, candidates[i].name) == 0)
         {
             return 0;
         }
