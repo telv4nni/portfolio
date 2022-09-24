446224957d5b4e2ae58573ad0104a140835b4b0c
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-24 17:26:08 +0000

    Sat, Sep 24, 2022, 5:26 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -154,7 +154,7 @@ void sort_pairs(void)
         for (int j = 0; j < pair_count; j++)
         {
             //if ((pairs[i].winner - pairs[i].loser) < (pairs[j].winner - pairs[j].loser))
-            if (preferences[pairs[i].winner][pairs[i].loser] < preferences[pairs[j].winner][pairs[j].loser])
+            if (preferences[pairs[i].winner][pairs[i].loser] < preferences[pairs[j].winner][pairs[j].loser] && (i != j))
             {
                 int winner;
                 int loser;
