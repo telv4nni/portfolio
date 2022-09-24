be850d93fcb5aa1af8079c671d63d5b06b154da0
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-09-24 16:47:20 +0000

    Sat, Sep 24, 2022, 4:47 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -173,13 +173,16 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    if (locked[j][pairs[i].loser] == true)
-    {
 
-    }
     for (int i = 0; i < pair_count; i++)
     {
-        locked[pairs[i].winner][pairs[i].loser] = true;
+        for (int j = 0; j < pair_count; j++)
+        {
+            if (locked[j][pairs[i].loser] != true)
+            {
+                locked[pairs[i].winner][pairs[i].loser] = true;
+            }
+        }
     }
     return;
 }
