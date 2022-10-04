b3906f91ea117309523c4442d36524ca42af9ae2
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-04 18:25:42 +0000

    Tue, Oct 4, 2022, 6:25 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,7 +34,8 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-
+    uint8_t header[HEADER_SIZE];
+    while (fread())
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
