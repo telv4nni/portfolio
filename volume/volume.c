b9a21849b8c7e89c08e154364360a167cf071a63
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-04 19:18:51 +0000

    Tue, Oct 4, 2022, 7:18 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,16 +35,16 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-    fread(header, sizeof(uint8_t), HEADER_SIZE, input);
-    fwrite(header, sizeof(uint8_t), HEADER_SIZE, output);
+    fread(header, HEADER_SIZE, 1, input);
+    fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    //while (fread(&buffer, sizeof(int16_t), 1, input))
-    //{
-        //buffer = buffer * factor;
-        //fwrite(&buffer, sizeof(int16_t), 1, output);
-    //}
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        buffer = buffer * factor;
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
     // Close files
     fclose(input);
     fclose(output);
