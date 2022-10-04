d3f5675ae893017cc9f7d50c41036fa3c5febfbf
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-04 18:34:53 +0000

    Tue, Oct 4, 2022, 6:34 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -40,7 +40,11 @@ int main(int argc, char *argv[])
         fwrite(&header, sizeof(uint8_t), HEADER_SIZE, output);
     }
     // TODO: Read samples from input file and write updated data to output file
-
+    int16_t buffer;
+    while (fread(&buffer, sizeof(int16_t), 1, input))
+    {
+        fwrite(&buffer, sizeof(int16_t), 1, output);
+    }
     // Close files
     fclose(input);
     fclose(output);
