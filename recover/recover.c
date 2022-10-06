054fd52e03270b909a845c23b5aa8125f7ff26fe
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-06 17:39:41 +0000

    Thu, Oct 6, 2022, 5:39 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,19 +6,27 @@ const int BLOCK_SIZE = 512;
 
 int main(int argc, char *argv[])
 {
+    //check number of arguments
     if (argc != 2)
     {
         printf("Use: ./recover *filetorecover*.raw");
         return 1;
     }
 
+    //load file
     FILE *file = fopen(argv[1], "r");
+    //check if you can read the file
     if (file == NULL)
     {
         printf("Could not open *file*.raw");
         return 1;
     }
-    
 
-    while (fread())
+    BYTE *datablock = malloc(BLOCK_SIZE);
+
+    //loop until image ends
+    while (fread(datablock, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
+    {
+
+    }
 }
