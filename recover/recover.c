6b01011c7c48f84232e2aafaa03abb6e35a6e6ea
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-06 17:21:00 +0000

    Thu, Oct 6, 2022, 5:21 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
+typedef uint8_t BYTE;
+
 int main(int argc, char *argv[])
 {
- 
+    if (argc != 2)
+    {
+        printf("Use: ./recover *filetorecover*.raw");
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    
 }
