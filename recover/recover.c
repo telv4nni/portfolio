e390164e9344c2fc614bcf2badb7c6a9587a8b2a
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-06 17:52:16 +0000

    Thu, Oct 6, 2022, 5:52 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,15 +24,17 @@ int main(int argc, char *argv[])
 
     BYTE *datablock = malloc(BLOCK_SIZE);
 
-    //loop until image ends
-    while (fread(datablock, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
+
+    //check if it's jpg
+    if (datablock[0] == 0xff && datablock[1] == 0xd8 && datablock[2] == 0xff)
     {
-        //check if it's jpg
-        if (datablock[0] == 0xff && datablock[1] == 0xd8 && datablock[2] == 0xff)
+        if (datablock[4] >= 0xe0 && datablock[4] <= 0xef)
         {
-            if (datablock[4] >= 0xe0 && datablock[4] <= 0xef)
+            //loop until image ends
+            while (fread(datablock, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
             {
-                fwrite(file)
+                //save recovered blocks to new file
+                fwrite(file, BLOCK_SIZE, 1, recoveredimage);
             }
         }
     }
