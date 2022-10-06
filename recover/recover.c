eb627b856e5755503d763af5fd0d0f5bec783f6c
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-06 18:18:06 +0000

    Thu, Oct 6, 2022, 6:18 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -29,7 +29,11 @@ int main(int argc, char *argv[])
     //loop through images
     for (int i = 0; i < sizeof(file); i++)
     {
-    FILE *recoveredimage = fopen(sprintf(&i, "%03i.jpg", 8), "w");
+    //create name of new file
+    string filename = malloc(8);
+    sprintf(i, "%03i.jpg", 8);
+    //open new file
+    FILE *recoveredimage = fopen(filename, "w");
         //check if it's jpg
         if (datablock[0] == 0xff && datablock[1] == 0xd8 && datablock[2] == 0xff)
         {
