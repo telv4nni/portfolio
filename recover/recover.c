1831c24ff4645d75dd37a1f08737f94d74649e36
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-06 19:28:58 +0000

    Thu, Oct 6, 2022, 7:28 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -29,34 +29,31 @@ int main(int argc, char *argv[])
     //create name of new file and file counter
     char filename[8];
     int counter = 0;
-
+    FILE *recoveredimage;
 
 
     //loop until file ends
     while (fread(datablock, 1, BLOCK_SIZE, file) == BLOCK_SIZE)
         {
             //check if it's jpg
-            if (datablock[0] == 0xff && datablock[1] == 0xd8 && datablock[2] == 0xff)
+            if (datablock[0] == 0xff && datablock[1] == 0xd8 && datablock[2] == 0xff && (datablock[3] & 0xf0) == 0xe0)
             {
-                if (datablock[3] >= 0xe0 && datablock[3] <= 0xef)
+                if (!(counter == 0))
                 {
-                    if (!(counter == 0))
-                    {
-                        fclose(recoveredimage);
-                    }
+                    fclose(recoveredimage);
+                }
 
-                    //change name of new file
-                    sprintf(filename, "%03i.jpg", counter);
+                //change name of new file
+                sprintf(filename, "%03i.jpg", counter);
 
-                    //open new file
-                    FILE *recoveredimage = fopen(filename, "w");
-                    counter++;
+                //open new file
+                recoveredimage = fopen(filename, "w");
+                counter++;
 
-                    //save recovered block to new file if jpg found
-                    if (!(counter == 0))
-                    {
-                        fwrite(&file, 1, BLOCK_SIZE, recoveredimage);
-                    }
+                //save recovered block to new file if jpg found
+                if (!(counter == 0))
+                {
+                    fwrite(&file, 1, BLOCK_SIZE, recoveredimage);
                 }
             }
         }
