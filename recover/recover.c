ba3bd80c1f199274109b2ac2a5fa7c52565130d3
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-06 17:54:25 +0000

    Thu, Oct 6, 2022, 5:54 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
     BYTE *datablock = malloc(BLOCK_SIZE);
 
-
+    FILE *recoveredimage = fopen(i, "w");
     //check if it's jpg
     if (datablock[0] == 0xff && datablock[1] == 0xd8 && datablock[2] == 0xff)
     {
