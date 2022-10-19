9b16b6c653b04e07fac4743a7edfa0ccd0de7c41
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-10-19 17:25:38 +0000

    Wed, Oct 19, 2022, 5:25 PM +00:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -18,6 +18,7 @@ node;
 
 // TODO: Choose number of buckets in hash table
 const unsigned int N = 26;
+int wordcount = 0;
 
 // Hash table
 node *table[N];
@@ -76,6 +77,7 @@ bool load(const char *dictionary)
         //be sure to set pointers in the correct order
         newnode->next = table[hashv]->next;
         table[hashv]->next = newnode;
+        wordcount++;
     }
 
 
@@ -87,7 +89,7 @@ bool load(const char *dictionary)
 unsigned int size(void)
 {
     // TODO
-    return 0;
+    return wordcount;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
