81f931311e7d7de8a3be3b0db983c67b0a9b9a26
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-11-26 14:16:45 +0000

    Sat, Nov 26, 2022, 2:16 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -1,9 +1,18 @@
-import re
 from cs50 import get_int
 
+
 def main():
+    number = getnumber()
+    checkcard()
+
+
 
 def getnumber():
     while True:
         number = get_int("Number: ")
-        if 
+        if int(number):
+            return number
+
+
+def checkcard(number):
+    
