bb7a0f708c76119818204f0e73f6d03073018990
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-11-26 15:49:31 +0000

    Sat, Nov 26, 2022, 3:49 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -12,14 +12,20 @@ def getnumber():
         number = input("Number: ")
         if int(number):
             return number
+            
 
-def addsum(even)
+def checkcard(creditnumber):
+    check = sum = 0
+    cardlen = len(creditnumber)
+
+    # Check if number of digits is even or odd
+    if cardlen % 2 == 0:
         # if number of digits is even start counting from first digit
         for i in range(cardlen):
             # Check if digit is odd
             # Assign digit to check number
             num = int(creditnumber[i])
-            if i % 2 == even:
+            if i % 2 == 0:
                 # Multiply odd number by 2
                 num = 2 * num
                 # Check if multiplied number is greater than 10
@@ -32,16 +38,6 @@ def addsum(even)
             else:
                 # add even number
                 sum += num
-        return sum;
-
-
-def checkcard(creditnumber):
-    check = sum = 0
-    cardlen = len(creditnumber)
-
-    # Check if number of digits is even or odd
-    if cardlen % 2 == 0:
-
     else:
         # if number of digits is odd, start counting from second digit
         for i in range(cardlen):
