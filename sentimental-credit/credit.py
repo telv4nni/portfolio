1b94b1b708335e2336b2326fb4ecbf388ab09704
telv4nni <112615622+telv4nni@users.noreply.github.com>
2022-11-26 15:02:10 +0000

    Sat, Nov 26, 2022, 3:02 PM +00:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -20,7 +20,7 @@ def checkcard(number):
 
     if cardlen % 2 == 0:
         for i in range(cardlen):
-            digit = int(2 * number[i])
+            digit = int(number[i - 1]) * 2
             #if digit > 10:
                 #sum = digit[0] + digit[1]
             #else:
