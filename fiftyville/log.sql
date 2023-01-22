371a4f46526af2128a442343988b92ef0de2bb4a
telv4nni <112615622+telv4nni@users.noreply.github.com>
2023-01-22 18:12:56 +0000

    Sun, Jan 22, 2023, 6:12 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -65,7 +65,7 @@ AND minute BETWEEN 5 AND 25
 AND activity = 'exit';
 
 --Great now let's check the phone calls
-SELECT caller, receiver, duration, month FROM phone_calls
+SELECT caller, receiver, duration FROM phone_calls
 WHERE caller IN
 (SELECT DISTINCT(phone_number) FROM people
 JOIN bakery_security_logs ON people.license_plate = bakery_security_logs.license_plate
@@ -93,4 +93,4 @@ AND activity = 'exit'))
 AND year = 2021
 AND month = 7
 AND day = 28
-AND duration < 60;
+AND duration <= 60;
