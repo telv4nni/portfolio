d2ff2f6d6e773ac94f73bf2e9b70712bdd2ea350
telv4nni <112615622+telv4nni@users.noreply.github.com>
2023-03-21 08:57:45 +0000

    Tue, Mar 21, 2023, 9:57 AM +01:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -159,10 +159,7 @@ SELECT name FROM people
     AND airports.abbreviation = 'CSF' ORDER BY hour LIMIT 1);
 
 -- Compare passengers with ATM
-SELECT name FROM atm_transactions
-JOIN bank_accounts ON atm_transactions.account_number = bank_accounts.account_number
-JOIN people ON bank_accounts.person_id = people.id
-WHERE year = 2021
+SELECT name FROM atm_transactions 
 AND month = 7
 AND day = 28
 AND atm_location LIKE '%Leggett%'
