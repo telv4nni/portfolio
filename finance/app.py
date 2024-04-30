24f6e11aeb68ae5ed137d8b3d9d8b4b614afc13e
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-04-30 12:12:44 +0000

    Tue, Apr 30, 2024, 2:12 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -126,11 +126,11 @@ def register():
         if password != confirmation:
             return apology("Passwords do not match")
     #generate password hash
-    hash = generate_password_hash('password')
-    #add username and hash to database
-    db.execute("INSERT INTO finance (username, hash) VALUES(?,?)", username, hash)
+    # hash = generate_password_hash('password')
+    # #add username and hash to database
+    # db.execute("INSERT INTO finance (username, hash) VALUES(?,?)", username, hash)
 
-    return redirect("/")
+    return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
