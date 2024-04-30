91e3c8ab4d82cfdfc39968252afe9b9e801e5b07
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-04-30 12:57:14 +0000

    Tue, Apr 30, 2024, 2:57 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -109,7 +109,8 @@ def quote():
     if request.method == "GET":
         return render_template("quote.html")
     if request.method == "POST":
-        
+        symbol = request.form.get("symbol")
+        lookup(symbol)
     return apology("TODO")
 
 
