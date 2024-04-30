07d3163292a81bc61afd020c494e1f6405a32875
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-04-30 11:29:08 +0000

    Tue, Apr 30, 2024, 1:29 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -112,7 +112,7 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
