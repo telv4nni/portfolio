6c43a03eac6d460acf6b7612e941e81db89764d6
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-05-03 13:11:50 +0000

    Fri, May 3, 2024, 3:11 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -36,8 +36,10 @@ def after_request(response):
 def index():
     """Show portfolio of stocks"""
     if request.method == "GET":
-        shares = db.execute("SELECT symbol,SUM(shares),SUM(price*shares) FROM shares WHERE user_id = ? GROUP BY symbol", session["user_id"])
-        return render_template("index.html", shares=shares)
+        shares = db.execute("SELECT symbol,SUM(shares), price, SUM(price*shares) FROM shares WHERE user_id = ? GROUP BY symbol", session["user_id"])
+        cash = db.execute("SELECT cash FROM users WHERE id = ?", session["user_id"])
+        cash = cash[0]['cash']
+        return render_template("index.html", shares=shares, cash=cash)
     return apology("TODO")
 
 
