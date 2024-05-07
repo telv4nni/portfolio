df973870f5e1183ae783c20347c1bb70c8b89e92
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-05-07 17:06:49 +0000

    Tue, May 7, 2024, 7:06 PM +02:00

--- finance/app.py
+++ finance/app.py
@@ -199,5 +199,5 @@ def sell():
             return apology("User doesn't own any share of this stock")
         sharesdb = db.execute("SELECT ?, SUM(shares) FROM shares WHERE user_id = ? GROUP BY symbol", symbol, session["user_id"])
         shares = request.form.get("shares")
-        if shares > sharesdb[sum(shares)]:
+        if shares > sharesdb.sum(shares):
             return apology("User does not have that many shares")
