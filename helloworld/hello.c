2c10325d1c7de05a7d6384bd97f8955cd06bf59c
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-08-08 18:05:54 +0000

    Thu, Aug 8, 2024, 8:05 PM +02:00

--- helloworld/hello.c
+++ helloworld/hello.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
-    string name = get_string("What's your name? ");
-    printf("Hello, %s!\n", name);
+    // string name = get_string("What's your name? ");
+    // printf("Hello, %s!\n", name);
+    printf("Hello, world);
 }
