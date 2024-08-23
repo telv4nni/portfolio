009dd41c5fc304425e76761524374885d32fb255
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-08-23 15:51:22 +0000

    Fri, Aug 23, 2024, 5:51 PM +02:00

--- integrals/integrals.py
+++ integrals/integrals.py
@@ -2,16 +2,16 @@ import csv
 import sys
 import random
 import math
-
+import integrals
 # Create Integrals Formulas for calculation
 
 # Count integral x^n dx
 def ixndx(x, power):
     x = x
     n = power
-    integral = None
-
-    integral = x^(n+1)/n+1
+    upper = x^(n+1)
+    lower = n+1
+    integral = string(upper + "/" + lower)
 
     return integral
 
