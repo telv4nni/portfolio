1cdb99cfc64228cf20f68967749be42ebaea48b5
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-08-23 15:38:22 +0000

    Fri, Aug 23, 2024, 5:38 PM +02:00

--- integrals/integrals.py
+++ integrals/integrals.py
@@ -5,12 +5,4 @@ import math
 
 def main():
 
-    # Ask user for input
 
-    # Select method
-
-    # Calculate integral
-
-    # Graphs
-
-    # Step by step solution
---
+++ integrals/main.py
@@ -0,0 +1,16 @@
+import csv
+import sys
+import random
+import math
+
+def main():
+
+    # Ask user for input
+
+    # Select method
+
+    # Calculate integral
+
+    # Graphs
+
+    # Step by step solution
