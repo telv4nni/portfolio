f1f2589ad7831bc0f5960c14a3f98fd65d592f6b
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-08-23 16:30:38 +0000

    Fri, Aug 23, 2024, 6:30 PM +02:00

--- integrals/integrals.py
+++ integrals/integrals.py
@@ -6,15 +6,19 @@ import math
 
 # Count integral x^n dx
 def ixndx(x, n):
+    x = x
+    n = n
     # Check if inputs are numbers
     ## x and n are defined:
     if type(x) == int or type(x) == float:
         if type(n) == int or type(n) == float:
-            x = x
-            n = n
             upper = pow(x, (n+1))
             lower = n+1
             integral = upper/lower
             return integral
     else:
+            upper = pow(x, (n+1))
+            lower = n+1
+            integral = (str)upper + "/" + (str)lower
+            return integral
         return("not defined")
