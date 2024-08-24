2cdd0224bee1da2d75d96b0435bfc1a49a7766d0
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-08-24 13:26:51 +0000

    Sat, Aug 24, 2024, 3:26 PM +02:00

--- integrals/integrals.py
+++ integrals/integrals.py
@@ -12,26 +12,13 @@ def iadx():
 # Count integral x^n dx
 def ixndx(x, n):
     # Check if inputs are numbers
-    ## x and n are defined:
-    if type(x) == int or type(x) == float:
-        if type(n) == int or type(n) == float:
-            upper = pow(x, (n+1))
-            lower = n+1
-            integral = upper/lower
-            return integral
-    ## x is defined and n is undefined
-        else:
-            upper = str(x) + "^" + n + "+1"
-            lower = n + " + 1"
-            integral = str(upper) + "/" + str(lower)
-            return integral
-    ## x is undefined and n is defined
-    elif type(n) == int or type(n) == float:
+    ## n is defined
+    if type(n) == int or type(n) == float:
             upper = "x^" + str(n + 1)
             lower = n+1
             integral = str(upper) + "/" + str(lower)
             return integral
-    ## x and n are undefined
+    ## n is undefined
     else:
             upper = str(x) + "^" + str(n) + "+1"
             lower = str(n) + "+1"
