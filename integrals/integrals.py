dbb48ea1dca1ab5aa04a45e32fc9d6283d991a7a
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-09-12 18:02:42 +0000

    Thu, Sep 12, 2024, 8:02 PM +02:00

--- integrals/integrals.py
+++ integrals/integrals.py
@@ -144,11 +144,11 @@ def integral_selector(expression):
           return ictgxdx('x')
 
      # if 1/cos^2(x)
-     elif re.match(r"1/cos\^2\(?x\)?", expression)
+     elif re.match(r"1/cos\^2\(?x\)?", expression):
           return i1cos2x('x')
 
      # if 1/sin^2(x)
-     elif re.match(r"1/cos\^2\(?x\)?", expression)
+     elif re.match(r"1/cos\^2\(?x\)?", expression):
           return i1sin2x('x')
 
      # if 1/(x^2 + a^2)
@@ -164,13 +164,13 @@ def integral_selector(expression):
           return i1x2a2m('x', a)
 
      # if 1/sqrt(a^2 - x^2)
-     elif re.match(r"1/sqrt\(\d+\^2-\^2\)", expression)
+     elif re.match(r"1/sqrt\(\d+\^2-\^2\)", expression):
           parts = expression.split("(")
           a = parts[1][0]
           return i1sqrta2x2m('x', a)
 
      # if 1/sqrt(x^2 + q)
-     elif re.match(r"1/sqrt\(x\^2\+\d+\)", expression)
+     elif re.match(r"1/sqrt\(x\^2\+\d+\)", expression):
           parts = expression.split("+")
           q = parts[1][0]
           return i1sqrtx2q('x', q)
