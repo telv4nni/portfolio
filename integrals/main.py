bc0e3806958e13e610e14f1c17c4c99d80c570d8
telv4nni <112615622+telv4nni@users.noreply.github.com>
2024-09-12 18:36:33 +0000

    Thu, Sep 12, 2024, 8:36 PM +02:00

--- integrals/main.py
+++ integrals/main.py
@@ -2,18 +2,18 @@ import csv
 import sys
 import math
 import integrals
-from sympy import symbols
 
-## TODO: add brackets when x is complex eg. x+1 -> (x+1)
 
 def main():
+
     # Ask user for input
     integral = input("Enter function you want to integrate: ")
-    final = integrals.integral_selector(integral)
-    print(f"The integral of the expression is: {final}")
-    # Select method
 
-    # Calculate integral
+    # Select method and calculate
+    final = integrals.integral_selector(integral)
+
+    # Print integral
+    print(f"The integral of the expression is: {final}")
 
     # Graphs
 
